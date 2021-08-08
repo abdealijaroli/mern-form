04ae13eb4b65fe484d4d61b8df8a88fa1074028f
abdealijaroli <abdealiburu03@gmail.com>
2021-08-08 17:58:02 +0530

    ui ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 const App = () => {
   return (
-    <div className="App">
-      <h2>MERN Form</h2>
+    <div className='container'>
+      <h2>Registration Form</h2>
+      <form className='form-control'>
+        <input type='text' name='name' placeholder='Name'></input>
+        <input type='email' name='email' placeholder='Email'></input>
+        <button type="submit" className='btn'>SUBMIT</button>
+      </form>
     </div>
   );
 }
